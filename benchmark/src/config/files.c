55eeda023f301657599f67de8e574afe56df43b4
Paul Hondola <paulhondola@gmail.com>
2025-12-27 19:55:22 +0200

    Refactor data directory structure and update build config
    Moved benchmark data files from 'data/chronos' to 'data' and updated references in the code. Added 'multithreaded_data.csv' and modified the Meson build file to update the project version, adjust warning level, and clean up run/plot targets.

--- benchmark/src/config/files.c
+++ benchmark/src/config/files.c
@@ -9,7 +9,7 @@
 const char *SINGLE_RUN_CSV_HEADER =
     "Pixel Count,Kernel Size,Clusters,Threads,Time";
 
-#define _DATA_FOLDER PROJECT_ROOT "data/chronos"
+#define _DATA_FOLDER PROJECT_ROOT "data"
 const char *SERIAL_CSV_FILE = _DATA_FOLDER "/serial_data.csv";
 const char *MULTITHREADED_CSV_FILE = _DATA_FOLDER "/multithreaded_data.csv";
 const char *DISTRIBUTED_CSV_FILE = _DATA_FOLDER "/distributed_data.csv";
