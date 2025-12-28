904b6b7b41c04ff9fe5f77640fbf53a91ea7e92a
Paul Hondola <paulhondola@gmail.com>
2025-12-29 00:49:21 +0200

    Add setup_project target
    Updated project and visualization versions to 0.9.6. Added a Meson run target for project setup and improved the setup_project.sh script with a shebang and corrected directory paths. Removed an unused include from file_utils.c.

--- benchmark/scripts/setup_project.sh
+++ benchmark/scripts/setup_project.sh
@@ -1,5 +1,7 @@
-mkdir -p ../images
-mkdir -p ../images/base
-mkdir -p ../data
-mkdir -p ../data/plots
-mkdir -p ../data/chrono
+#!/bin/bash
+
+mkdir -p "../../images"
+mkdir -p "../../images/base"
+mkdir -p "../../data"
+mkdir -p "../../data/plots"
+mkdir -p "../../data/chrono"
